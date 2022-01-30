b72e18b6f3a53121b7769d45f8fd645d7806a754
Modris <modris.martinovs@gmail.com>
2022-01-30 23:00:56 +0200

    ChatBot application sketch.

--- eu/modris/ChatBotMain.java
+++ eu/modris/ChatBotMain.java
@@ -15,7 +15,7 @@ public class ChatBotMain {
         while (true) {
 
             String text = scanner.nextLine();
-            if ("q".equals(text)) {
+            if ("Paldies.Uz redzēšanos.".equals(text)) {
                 System.out.println("Uz redzēšanos!");
                 break;
 
@@ -23,13 +23,20 @@ public class ChatBotMain {
                 System.out.println("Mēs piedāvājam Java, Python un JavaScript kursus. " + "Kādi jūs interesētu?");
 
             } else if ("Man interesē Java kursi.".equalsIgnoreCase(text)) {
-                System.out.println("Mēs piedāvajam kursu 8.februārī");
+                System.out.println("Mēs piedāvajam kursu 8.februārī.");
+
+            } else if ("Vai es varētu pieteikties uz šo kursu?".equalsIgnoreCase(text)){
+                System.out.println("Jā, Protams");
+
+            } else if ("Sakiet, kursi notiek klātienē vai attālināti?".equalsIgnoreCase(text)) {
+                System.out.println("Kursi šobrīd notiek attālināti.");
 
             } else {
                 System.out.println("Atvainojiet, es nevaru atbildēt uz šo jautājumu!");
             }
 
         }
+
 
 
 
