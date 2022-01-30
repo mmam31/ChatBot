e8e13fa7d96e46b73d1335ee07e3cf7e3efa830c
Modris <modris.martinovs@gmail.com>
2022-01-30 23:38:52 +0200

    ChatBot application final!

--- eu/modris/ChatBotMain.java
+++ eu/modris/ChatBotMain.java
@@ -6,16 +6,22 @@ import java.util.Scanner;
 public class ChatBotMain {
 
     public static void main(String[] args) {
+
         String hello = "Labdien! Jūs uzrakstījāt RigaCodingSchool!";
         String welcomeQuestion = "Kā es varu Jums palīdzēt?";
+
         String question1 = "Kādus programmēšanas kursus Jūs pasniedzat?";
         String answer1 = "Mēs piedāvājam Java, Python un JavaScript kursus. Kādi jūs interesētu?";
+
         String question2 = "Man interesē Java kursi.";
         String answer2 = "Mēs piedāvajam tuvāko Java kursu 8.februārī.";
+
         String question3 = "Vai es varētu pieteikties uz šo kursu?";
         String answer3 = "Jā, Protams";
+
         String question4 = "Sakiet, kursi notiek klātienē vai attālināti?";
         String answer4 = "Visi kursi šobrīd notiek attālināti.";
+
         String question5 = "Cik ilgi kursi ies?";
         String answer5 = "Kursi ies 2 mēnešus";
 
